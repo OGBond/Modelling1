61e67882dc3afef38736425b4e6e4a166fd04b7a
huining yang <yang@maths.ox.ac.uk>
2018-10-10 19:24:41 +0100

    Four silmulation pictures

--- Matlab code/Same Radius/radiusalpha.m
+++ Matlab code/Same Radius/radiusalpha.m
@@ -1,10 +1,12 @@
 % Plot the figure of R_m versus alpha
-alpha = linspace(-pi,pi,100);
+alpha = linspace(-pi/2,pi/2,80);
 l = 4e-6;
-theta = 1*pi/6;
+theta = 1*pi/3;
 R = 1e-6;
 R_m = -(l/2-R*cos(alpha))./cos(-alpha+theta);
-plot(alpha,R_m)
-xlabel('alpha')
+plot(alpha*180/pi,R_m)
+xlabel('alpha (degree)')
 ylabel('R_m')
 title('Relationship between R_m and alpha')
+xlim([-90,90])
+shg
